87c47b4b582d67725bc8e22d35c6aba3b0ddb26f
Faiza-Mushtari <80122756+Faiza-Mushtari@users.noreply.github.com>
2023-04-13 02:31:34 +0600

    Add src files

---
+++ registration-login-spring-boot-security-thymeleaf-hibernate/src/main/java/net/javaguides/springboot/web/UserRegistrationController.java
@@ -0,0 +1,36 @@
+package net.javaguides.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import net.javaguides.springboot.service.UserService;
+import net.javaguides.springboot.web.dto.UserRegistrationDto;
+
+@Controller
+@RequestMapping("/registration")
+public class UserRegistrationController {
+
+	private UserService userService;
+
+	public UserRegistrationController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+	
+	@ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
+    }
+	
+	@GetMapping
+	public String showRegistrationForm() {
+		return "registration";
+	}
+	
+	@PostMapping
+	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+		userService.save(registrationDto);
+		return "redirect:/registration?success";
+	}
+}
